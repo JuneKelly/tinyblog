3af546319339f851c6c16f37e21590bfe5351e6a
Shane Kilkelly <shanekilkelly@gmail.com>
2013-06-27 18:20:55 +0100

    retrieve blog post details

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,11 @@ from django_mongokit import get_database
 def index(request):
     coll = get_database()['blogposts']
     posts = coll.BlogPost.find()
+    
     return render(request, 'blog/post_list.html', {'posts': posts}) 
 
 def blog_post(request, slug):
-    return HttpResponse('blog slug = {0}'.format(slug))
+    coll = get_database()['blogposts']
+    post = coll.BlogPost.find_one({ 'slug': slug })
+    
+    return HttpResponse('blog = {0}'.format(post['title']))
